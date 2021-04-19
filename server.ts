b222bfcebe905991aee18f8d93f479d44ddd62e1
blakeoxx <blakeoxx@netscape.net>
2021-04-19 12:50:08 -0500

    added webpack

--- server.ts
+++ server.ts
@@ -53,11 +53,15 @@ function run() {
 // Webpack will replace 'require' with '__webpack_require__'
 // '__non_webpack_require__' is a proxy to Node 'require'
 // The below code is to ensure that the server is run only when not requiring the bundle.
-declare const __non_webpack_require__: NodeRequire;
+/*declare const __non_webpack_require__: NodeRequire;
 const mainModule = __non_webpack_require__.main;
 const moduleFilename = mainModule && mainModule.filename || '';
 if (moduleFilename === __filename || moduleFilename.includes('iisnode')) {
+  console.log('Webpack check passed. Server can be run');
   run();
-}
+} else {
+  console.log('Webpack check FAILED. Server will NOT run');
+}*/
+run();
 
 export * from './src/main.server';
---
+++ webpack.server.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    // This is our Express server for Dynamic universal
+    server: './server.ts'
+  },
+  target: 'node',
+  mode: 'development',
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  // Make sure we include all node_modules etc
+  externals: [/(node_modules|main\..*\.js)/],
+  output: {
+    // Puts the output at the root of the dist folder
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js'
+  },
+  module: {
+    rules: [{
+      test: /\.tsx?$/,
+      use: 'ts-loader'
+    }]
+  },
+};
